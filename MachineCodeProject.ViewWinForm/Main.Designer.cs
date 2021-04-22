b4f62bee2879aa1543cc50c5c44d36e404223f72
Роман <kuroma201@hotmail.com>
2021-04-22 17:28:18 +0300

    Изменен интерфейс Form. Добавлен рандом чисел от -100 до 100

--- MachineCodeProject.ViewWinForm/Main.Designer.cs
+++ MachineCodeProject.ViewWinForm/Main.Designer.cs
@@ -35,13 +35,16 @@ namespace MachineCodeProject.ViewWinForm
             this.richTextBoxResult = new System.Windows.Forms.RichTextBox();
             this.labelToTranslateTB = new System.Windows.Forms.Label();
             this.labelToResultTB = new System.Windows.Forms.Label();
+            this.labelToRandomTB = new System.Windows.Forms.Label();
+            this.textBoxRandomCount = new System.Windows.Forms.TextBox();
+            this.buttonTranslateRandom = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonTranslate
             // 
-            this.buttonTranslate.Location = new System.Drawing.Point(466, 44);
+            this.buttonTranslate.Location = new System.Drawing.Point(12, 106);
             this.buttonTranslate.Name = "buttonTranslate";
-            this.buttonTranslate.Size = new System.Drawing.Size(137, 30);
+            this.buttonTranslate.Size = new System.Drawing.Size(246, 30);
             this.buttonTranslate.TabIndex = 0;
             this.buttonTranslate.Text = "Перевести";
             this.buttonTranslate.UseVisualStyleBackColor = true;
@@ -49,26 +52,26 @@ namespace MachineCodeProject.ViewWinForm
             // 
             // buttonCopyResult
             // 
-            this.buttonCopyResult.Location = new System.Drawing.Point(466, 151);
+            this.buttonCopyResult.Location = new System.Drawing.Point(12, 325);
             this.buttonCopyResult.Name = "buttonCopyResult";
-            this.buttonCopyResult.Size = new System.Drawing.Size(137, 58);
+            this.buttonCopyResult.Size = new System.Drawing.Size(276, 58);
             this.buttonCopyResult.TabIndex = 1;
-            this.buttonCopyResult.Text = "Копировать результат";
+            this.buttonCopyResult.Text = "Копировать результаты";
             this.buttonCopyResult.UseVisualStyleBackColor = true;
             this.buttonCopyResult.Click += new System.EventHandler(this.buttonCopyResult_Click);
             // 
             // textBoxNumeric
             // 
-            this.textBoxNumeric.Location = new System.Drawing.Point(264, 44);
+            this.textBoxNumeric.Location = new System.Drawing.Point(12, 70);
             this.textBoxNumeric.Name = "textBoxNumeric";
-            this.textBoxNumeric.Size = new System.Drawing.Size(160, 30);
+            this.textBoxNumeric.Size = new System.Drawing.Size(246, 30);
             this.textBoxNumeric.TabIndex = 2;
             // 
             // richTextBoxResult
             // 
-            this.richTextBoxResult.Location = new System.Drawing.Point(12, 151);
+            this.richTextBoxResult.Location = new System.Drawing.Point(340, 54);
             this.richTextBoxResult.Name = "richTextBoxResult";
-            this.richTextBoxResult.Size = new System.Drawing.Size(412, 430);
+            this.richTextBoxResult.Size = new System.Drawing.Size(412, 527);
             this.richTextBoxResult.TabIndex = 3;
             this.richTextBoxResult.Text = "";
             // 
@@ -84,17 +87,46 @@ namespace MachineCodeProject.ViewWinForm
             // labelToResultTB
             // 
             this.labelToResultTB.AutoSize = true;
-            this.labelToResultTB.Location = new System.Drawing.Point(12, 111);
+            this.labelToResultTB.Location = new System.Drawing.Point(340, 18);
             this.labelToResultTB.Name = "labelToResultTB";
             this.labelToResultTB.Size = new System.Drawing.Size(170, 23);
             this.labelToResultTB.TabIndex = 5;
             this.labelToResultTB.Text = "Результат перевода:";
             // 
+            // labelToRandomTB
+            // 
+            this.labelToRandomTB.AutoSize = true;
+            this.labelToRandomTB.Location = new System.Drawing.Point(12, 199);
+            this.labelToRandomTB.Name = "labelToRandomTB";
+            this.labelToRandomTB.Size = new System.Drawing.Size(276, 23);
+            this.labelToRandomTB.TabIndex = 6;
+            this.labelToRandomTB.Text = "Введите количество чисел(<201):";
+            // 
+            // textBoxRandomCount
+            // 
+            this.textBoxRandomCount.Location = new System.Drawing.Point(12, 225);
+            this.textBoxRandomCount.Name = "textBoxRandomCount";
+            this.textBoxRandomCount.Size = new System.Drawing.Size(276, 30);
+            this.textBoxRandomCount.TabIndex = 7;
+            // 
+            // buttonTranslateRandom
+            // 
+            this.buttonTranslateRandom.Location = new System.Drawing.Point(12, 261);
+            this.buttonTranslateRandom.Name = "buttonTranslateRandom";
+            this.buttonTranslateRandom.Size = new System.Drawing.Size(276, 30);
+            this.buttonTranslateRandom.TabIndex = 8;
+            this.buttonTranslateRandom.Text = "Перевести числа";
+            this.buttonTranslateRandom.UseVisualStyleBackColor = true;
+            this.buttonTranslateRandom.Click += new System.EventHandler(this.buttonTranslateRandom_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(675, 593);
+            this.ClientSize = new System.Drawing.Size(779, 593);
+            this.Controls.Add(this.buttonTranslateRandom);
+            this.Controls.Add(this.textBoxRandomCount);
+            this.Controls.Add(this.labelToRandomTB);
             this.Controls.Add(this.labelToResultTB);
             this.Controls.Add(this.labelToTranslateTB);
             this.Controls.Add(this.richTextBoxResult);
@@ -117,6 +149,9 @@ namespace MachineCodeProject.ViewWinForm
         private System.Windows.Forms.RichTextBox richTextBoxResult;
         private System.Windows.Forms.Label labelToTranslateTB;
         private System.Windows.Forms.Label labelToResultTB;
+        private System.Windows.Forms.Label labelToRandomTB;
+        private System.Windows.Forms.TextBox textBoxRandomCount;
+        private System.Windows.Forms.Button buttonTranslateRandom;
     }
 }
 
--- MachineCodeProject.ViewWinForm/Main.cs
+++ MachineCodeProject.ViewWinForm/Main.cs
@@ -17,7 +17,7 @@ namespace MachineCodeProject.ViewWinForm
             try
             {
                 if (string.IsNullOrWhiteSpace(textBoxNumeric.Text))
-                    throw new ArgumentException("Переводимое число не может быть пустым или равно null.", nameof(textBoxNumeric.Text));
+                    throw new ArgumentException("Переводимое число не может быть пустым или равно null.", nameof(textBoxNumeric));
     
                 MachineCodeClass mc = TranslateInCode.TranslateNumericInMachineCode(Int32.Parse(textBoxNumeric.Text));
 
@@ -34,6 +34,35 @@ namespace MachineCodeProject.ViewWinForm
         private void buttonCopyResult_Click(object sender, EventArgs e)
         {
             Clipboard.SetText(richTextBoxResult.Text);
+        }
+
+        private void buttonTranslateRandom_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                #region Проверка на ошибки
+                if (string.IsNullOrWhiteSpace(textBoxRandomCount.Text))
+                    throw new ArgumentNullException("Поле \"Введите количество чисел\" должно быть заполнено.", nameof(textBoxRandomCount));
+
+                if (Convert.ToInt32(textBoxRandomCount.Text) > 200)
+                    throw new ArgumentException("Число в поле \"Введите количество чисел\" должно быть меньше 201.", nameof(textBoxRandomCount));
+                #endregion
+
+                richTextBoxResult.Clear();
+                Random rnd = new Random();
+                MachineCodeClass mc;
+                for (int i = 0; i < Convert.ToInt32(textBoxRandomCount.Text); i++)
+                {
+                    mc = TranslateInCode.TranslateNumericInMachineCode(rnd.Next(-100, 100));
+                    richTextBoxResult.Text += mc.ToString() + "\n\n";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+          
         }
     }
 }
